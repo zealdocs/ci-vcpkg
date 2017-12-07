022d0814af228e4f999b3a84fcef81251293fa77
Oleg Shparber <trollixx@gmail.com>
2017-12-08 01:36:47 +0200

    Add make.ps1 and appveyor.yml

---
+++ make.ps1
@@ -0,0 +1,32 @@
+param(
+    [Parameter(Mandatory=$True)]
+    [ValidateSet('x86', 'x64')]
+    [string]$platform,
+
+    [Parameter(Mandatory=$True)]
+    [string]$version
+)
+
+# Patch vcpkg
+$patches = Get-ChildItem .\patches\*.patch
+foreach ($patch in $patches) {
+    git apply $patch --directory vcpkg
+}
+
+# Bootstrap vcpkg
+Set-Location -Path vcpkg
+.\bootstrap-vcpkg.bat
+
+# Install Zeal dependencies
+$triplet = "$platform-windows"
+$dependencies = @("libarchive:$triplet", "openssl:$triplet", "sqlite3:$triplet")
+.\vcpkg install $dependencies
+
+# Export vcpkg toolchain
+.\vcpkg export --raw $dependencies
+Get-ChildItem "vcpkg-export-*" | Rename-Item -NewName "vcpkg-export"
+.\vcpkg list > .\vcpkg-export\package-list.txt
+7z a "vcpkg-export-$version-$platform.7z" ".\vcpkg-export\*"
+
+# Done here
+Set-Location -Path ..
